320bf9e7f5c3ffdf5a132535f83e00485f6d13de
Varun Hasija <varun.hasija@doceree.com>
2025-12-26 16:40:07 +0530

    netlify build fix

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-616e068a201ad621.js","static/chunks/fd9d1056-b11b2651f33aae7f.js","static/chunks/117-68a967612ebc79ed.js","static/chunks/main-app-4b893491f5246c5a.js"],pages:{"/_app":["static/chunks/webpack-616e068a201ad621.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-4349426c43e3e850.js","static/chunks/pages/_app-72b849fbd24ac258.js"],"/_error":["static/chunks/webpack-616e068a201ad621.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-4349426c43e3e850.js","static/chunks/pages/_error-7ba65e1336b92748.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
